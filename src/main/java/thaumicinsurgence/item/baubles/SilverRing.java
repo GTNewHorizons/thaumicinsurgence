77193fa3b1dc111607b5b826e490b7d78e1335f9
UltraProdigy <187078471+UltraProdigy@users.noreply.github.com>
2026-03-16 14:25:50 -0400

    Optimize Images & Workflow (#46)

--- src/main/java/thaumicinsurgence/item/baubles/SilverRing.java
+++ src/main/java/thaumicinsurgence/item/baubles/SilverRing.java
@@ -3,7 +3,6 @@ package thaumicinsurgence.item.baubles;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 
-import baubles.common.items.ItemRing;
 import thaumcraft.api.IVisDiscountGear;
 import thaumcraft.api.aspects.Aspect;
 
