cbf4b0289c28b198e930cc4caabc81cfeea6076a
Mary Hopson <doyoumined@gmail.com>
2026-06-08 17:27:23 -0400

    Remove desugar

--- src/main/java/thaumicinsurgence/api/VisweaverRecipe.java
+++ src/main/java/thaumicinsurgence/api/VisweaverRecipe.java
@@ -2,9 +2,6 @@ package thaumicinsurgence.api;
 
 import net.minecraft.item.ItemStack;
 
-import com.github.bsideup.jabel.Desugar;
-
 import thaumcraft.api.aspects.Aspect;
 
-@Desugar
 public record VisweaverRecipe(Aspect aspect, int cost, ItemStack input, ItemStack output) {}
